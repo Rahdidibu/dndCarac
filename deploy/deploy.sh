424ca59199d491f5e52233afb459b67a740b2191
Rahdidibu <matt35231@gmail.com>
2026-07-31 15:24:45 +0200

    Edit docker compose for server

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -11,8 +11,8 @@ echo "🔄 Récupération des dernières modifications depuis Git..."
 git pull
 
 echo "🛠️ Relance et reconstruction du conteneur Docker (sans cache)..."
-docker compose -f deploy/docker-compose.yml down
-docker compose -f deploy/docker-compose.yml build --no-cache
-docker compose -f deploy/docker-compose.yml up -d
+docker-compose -f deploy/docker-compose.yml down
+docker-compose -f deploy/docker-compose.yml build --no-cache
+docker-compose -f deploy/docker-compose.yml up -d
 
 echo "✅ Mise à jour terminée ! L'application est disponible sur le port 8080."
